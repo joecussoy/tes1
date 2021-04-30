53e642f9c02872ad948d9ca5d25634e87978f181
joecussoy <jonathancussoy@gmail.com>
2021-04-30 19:33:01 +0700

    Initial Commit

---
+++ tes.js
@@ -0,0 +1,7 @@
+function changetitle(){
+    document.getElementById("title1").innerHTML =  document.getElementById("input1").value
+}
+
+function changecolor(warna){
+    document.getElementById("title1").style.color = `${warna}`
+}
